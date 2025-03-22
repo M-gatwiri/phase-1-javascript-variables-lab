b64c113e53436632fe331be6194a3a1ebac85dc3
M-gatwiri <mgatwiri806@gmail.com>
2025-03-22 10:30:24 +0300

    Practising Variables

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+ // Code your solution in this file!
+
+// Declaring cons
+//  Using a string
+const userName =  "Mercy"
+console.log (userName);
+
+// Using an array
+// An array uses curly brackets
+const topNumbers =  [1,2,3];
+
+// Using boolean
+const isSkyBlue = true;
+
+// Using integers
+const age =  25;
+
+
+// Declaring Let
+// Using a String
+let student =  "Mercy"
+
+let city =  ['Nairobi','Mombasa','Kisumu'];
+
+let favouriteColorBlue =  true;
+
+let shoeSize =  5;
